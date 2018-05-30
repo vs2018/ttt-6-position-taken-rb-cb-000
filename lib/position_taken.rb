88d3669a1446cd4d3c7e476cc3c5124a23768f1f
Vishal Sharma <vsh1819@gmail.com>
2018-05-30 09:07:31 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -10,12 +10,7 @@
 def position_taken?(board, index)
   if(board[index] == " " || board[index] == "" || board[index] == nil)
     return false
-  # elsif(board[index] == "")
-  #   return false
-  # elsif(board[index] == nil)
-  #   return false
   else
     return true
   end
-  
 end
